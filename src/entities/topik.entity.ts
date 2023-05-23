cf1a564d1963ece72e8d84dcf5744a4b792f9a7c
Akmaljon571 <akmaljon079@gmail.com>
2023-05-23 13:43:40 +0300

    course update database

--- src/entities/topik.entity.ts
+++ src/entities/topik.entity.ts
@@ -7,6 +7,7 @@ import {
 } from 'typeorm';
 import { TakeEntity } from './take.entity';
 import { VideoEntity } from './video.entity';
+import { Sequence } from 'src/types';
 
 @Entity()
 export class TopikEntity extends BaseEntity {
@@ -39,11 +40,12 @@ export class TopikEntity extends BaseEntity {
   image: string;
 
   @Column({
-    type: 'integer',
+    type: 'enum',
     nullable: false,
+    default: Sequence.A,
     unique: true,
   })
-  sequence: number;
+  sequence: Sequence;
 
   @OneToMany(() => VideoEntity, (video) => video.topik)
   topik_videos: VideoEntity[];
