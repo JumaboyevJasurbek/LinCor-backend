5ccae55a60022b0baac98b57c3830a452109369b
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2023-06-01 00:30:32 -0700

    fixed bugs in the user_discount

--- src/module/discount/dto/create-discount.dto.ts
+++ src/module/discount/dto/create-discount.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { CourseEntity } from 'src/entities/course.entity';
 import { Discount } from 'src/entities/discount.entity';
@@ -6,8 +7,20 @@ import { DeepPartial } from 'typeorm';
 export class CreateDiscountDto {
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    name: 'percentage',
+    type: 'number',
+    default: 20,
+    required: true,
+  })
   percentage: number;
 
-  // @IsString()
+  @IsString()
+  @ApiProperty({
+    name: 'course_id',
+    type: 'string',
+    default: '02d0de60-462b-42a8-b9f3-e46d52c910e1',
+    required: true,
+  })
   course_id: any | string;
 }
